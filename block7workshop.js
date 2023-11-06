6a53e12287db6814dbf1fd2fd9e291cb5c83cd2a
Jayme Swanson <jaymeswanson@Jaymes-MacBook-Air.local>
2023-11-06 14:36:10 -0500

    Block 7 workshop finished

---
+++ block7workshop.js
@@ -0,0 +1,14 @@
+//Combination of lock for the vault is 10 - 40 - 39
+//Each combination number needs its own arithmetic equation
+//combination will be in html page
+//pop up on html page will say You have received this message because you have been chosen to open an important vault. Here is the secret combination: followed by equations
+//there will be three variables with an equation equaling out to the vault numbers
+
+const str = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination';
+
+var num = 5 + 5;
+//combination will never change, so const was used
+const num2 = 20 * 2;
+//combination will never change, so const was used
+const num3 = 45 - 6;
+
